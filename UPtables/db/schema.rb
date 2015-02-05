ec1eaf32f72d2b44110daffdb36ae580677223eb
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-05 01:03:42 +0100

    added room model

--- UPtables/db/schema.rb
+++ UPtables/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201111733) do
+ActiveRecord::Schema.define(version: 20150205000256) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "capacity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "timetable_entries", force: :cascade do |t|
     t.string   "course"
