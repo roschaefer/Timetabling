bc21ca4c0b4995867e7eaadf5b690bdbe6580831
Felix Kubicek <kubicek@uni-potsdam.de>
2015-02-09 17:25:25 +0100

    integers instead of foreign keys

--- UPtables/db/migrate/20150209145740_create_unavailabilities.rb
+++ UPtables/db/migrate/20150209145740_create_unavailabilities.rb
@@ -1,14 +1,11 @@
 class CreateUnavailabilities < ActiveRecord::Migration
   def change
     create_table :unavailabilities do |t|
-      t.references :teacher, index: true
-      t.references :weekday, index: true
-      t.references :timeframe, index: true
+      t.integer :teacher
+      t.integer :weekday
+      t.integer :timeframe
 
       t.timestamps null: false
     end
-    add_foreign_key :unavailabilities, :teachers
-    add_foreign_key :unavailabilities, :weekdays
-    add_foreign_key :unavailabilities, :timeframes
   end
 end
