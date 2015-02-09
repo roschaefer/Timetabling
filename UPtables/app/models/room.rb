b962cb6d2ae9346fe293cdb8e26ddf5dad846a17
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-09 17:51:43 +0100

    \#to_fact model specs are green

--- UPtables/app/models/room.rb
+++ UPtables/app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ActiveRecord::Base
   has_many :unavailabilities
   validates :name, presence: true
+
+
+  def to_fact
+    "room(#{id}, #{capacity}, \"Building\")"
+  end
 end
--- UPtables/app/models/room/unavailability.rb
+++ UPtables/app/models/room/unavailability.rb
@@ -2,4 +2,8 @@ class Room::Unavailability < ActiveRecord::Base
   belongs_to :room
   belongs_to :weekday
   belongs_to :timeframe
+
+  def to_fact
+    "room_unavailability(#{room.id}, #{weekday.id}, #{timeframe.id})"
+  end
 end
