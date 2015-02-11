e1e2b619fe7beab5f935ebbbca6c446ba7f17626
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-11 23:27:29 +0100

    added test cases for availability methods

--- UPtables/spec/models/room_spec.rb
+++ UPtables/spec/models/room_spec.rb
@@ -1,10 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  let(:room) { create :room, :id => 1, :name => "Raum 01", :capacity => 42 }
+
   describe "#to_fact" do
     it "should encode its state to an ASP fact" do
-      room = create :room, :id => 1, :name => "Raum 01", :capacity => 42
       expect(room.to_fact).to eq("room(1, 42, \"Building\").")
     end
   end
+
+
+  describe "#unavailable_at!" do
+    let(:monday) { create :weekday, :id => 0}
+    let(:eleven) { create :timeframe, :id => 1}
+
+    it "creates a new Unavailability" do
+      expect{
+        room.unavailable_at! monday, eleven
+      }.to change{Room::Unavailability.count}.from(0).to(1)
+    end
+
+    it "will be unavailable at a certain time" do
+      room.unavailable_at! monday, eleven
+      expect(room.unavailabilities.first.weekday).to eq monday
+      expect(room.unavailabilities.first.timeframe).to eq eleven
+    end
+
+    it "doesn't create duplicates" do
+      room.unavailable_at! monday, eleven
+      expect{
+        room.unavailable_at! monday, eleven
+      }.not_to change{Room::Unavailability.count}
+    end
+  end
+
+
+  describe "#available_at!" do
+    let(:monday) { create :weekday, :id => 0}
+    let(:eleven) { create :timeframe, :id => 1}
+
+    it "deletes an Unavailability at this time" do
+      Room::Unavailability.create!(:room_id => room.id, :weekday_id => 0, :timeframe_id =>1)
+      expect{
+        room.available_at! monday, eleven
+      }.to change{Room::Unavailability.count}.from(1).to(0)
+    end
+  end
+
 end
