32bfa2a7c7d4e2c5a2263b89a172edb555e691ca
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-11 23:54:54 +0100

    added pry, implemented model methods

--- UPtables/app/models/room.rb
+++ UPtables/app/models/room.rb
@@ -6,4 +6,14 @@ class Room < ActiveRecord::Base
   def to_fact
     "room(#{id}, #{capacity}, \"Building\")."
   end
+
+
+  def available_at!(weekday, timeframe)
+    Room::Unavailability.where(:room => self, :weekday => weekday, :timeframe => timeframe).destroy_all
+  end
+
+  def unavailable_at!(weekday, timeframe)
+    Room::Unavailability.where(:room => self, :weekday => weekday, :timeframe => timeframe).first_or_create
+  end
+
 end
