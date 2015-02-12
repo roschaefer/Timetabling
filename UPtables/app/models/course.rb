6eb9f459b47b352b176d14ee2469463bf188ae2c
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-12 22:02:56 +0100

    implemented to_fact in course

--- UPtables/app/models/course.rb
+++ UPtables/app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :teacher
   def to_fact
-    ""
-
+    dl = (double_lecture && "1") || "0"
+    "course(\"#{name}\",\"#{teacher.full_name}\",#{dates},#{minimum_working_days},#{participants},#{dl})."
   end
 end
--- UPtables/app/models/teacher.rb
+++ UPtables/app/models/teacher.rb
@@ -1,3 +1,7 @@
 class Teacher < ActiveRecord::Base
   has_many :unavailabilities, dependent: :destroy
+
+  def full_name
+    "#{first_name} #{surname}"
+  end
 end
