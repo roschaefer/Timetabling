675ebba06590d80ca507e625217c322329f8b903
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-13 19:39:18 +0100

    added asp::model specs

---
+++ UPtables/spec/integration/model_spec.rb
@@ -0,0 +1,17 @@
+describe Asp::Model do
+  describe "#extract" do
+    let(:value_hash) { {"Value" => ["assigned(3,2,4,1)"]} }
+    subject(:model) { Asp::Model.new(value_hash) }
+    it "extracts ruby objects" do
+      expect(model.extract Timetable::Entry ).to have(1).item
+    end
+
+    it "extracts timetable entries and assignes attributes" do
+      entry = model.extract(Timetable::Entry).first
+      expect(entry.course).to eq "3"
+      expect(entry.room_id).to eq 2
+      expect(entry.weekday_id).to eq 4
+      expect(entry.timeframe_id).to eq 1
+    end
+  end
+end
--- UPtables/spec/integration/solver_spec.rb
+++ UPtables/spec/integration/solver_spec.rb
@@ -1,22 +1,24 @@
 describe Asp::Solver do
   subject(:solver) { Asp::Solver.new }
-  describe "#solve" do
+  describe "#models" do
+    subject(:models) {solver.models}
     context "for a given ASP encoding" do
 
       it "with one fact yields one stable model" do
         solver.solve "this."
-        expect(solver.models).to have(1).item
+        expect(models).to have(1).item
       end
 
       it "with a decision rule yields two stable models" do
         solver.solve "a:- not b. b:- not a."
-        expect(solver.models).to have(2).item
+        expect(models).to have(2).items
       end
 
       it "with invalid syntax yields no stable models" do
         solver.solve "this"
-        expect(solver.models).to be_empty
+        expect(models).to be_empty
       end
+
     end
   end
 end
