a4bbacc2d72a3e83229d5e20782c95d31409c3e1
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-16 14:24:42 +0100

    fixed cucumber step definitions

--- UPtables/features/step_definitions/steps.rb
+++ UPtables/features/step_definitions/steps.rb
@@ -3,17 +3,18 @@ Angenommen(/^es gibt einen Kurs/) do
 end
 
 Angenommen(/^es gibt noch einen anderen Lehrer in der Datenbank$/) do
-  @another_teacher = create :teacher
+  @another_teacher = create :teacher, :first_name => "I am another", :surname => "Teacher"
 end
 
 Wenn(/^ich auf die Seite vom Kurs gehe$/) do
-  visit course_path(Course.first)
+  visit edit_course_path(Course.first)
 end
 
 Wenn(/^den ursprünglichen Lehrer gegen den anderen austausche$/) do
-  page.select @another_teacher.name, :from => 'Teacher'
+  page.select @another_teacher.full_name, :from => "course[teacher_id]"
+  click_button 'Save'
 end
 
 Dann(/^sollte der Kurs in der Datenbank dem neuen Lehrer zugeordnet sein$/) do
-  expect(Course.first.teacher).to be @another_teacher
+  expect(Course.first.teacher).to eq @another_teacher
 end
