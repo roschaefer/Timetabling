5ca504129561625e895ad85dda9d2e8d3a3b6465
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-16 14:24:57 +0100

    implemented cucumber feature for teachers<->courses

--- UPtables/app/controllers/courses_controller.rb
+++ UPtables/app/controllers/courses_controller.rb
@@ -69,6 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name, :dates, :minimum_working_days, :participants, :double_lecture)
+      params.require(:course).permit(:name, :teacher_id, :dates, :minimum_working_days, :participants, :double_lecture)
     end
 end
