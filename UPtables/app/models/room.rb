2c78e9913f07b342aa0c17b17b13ac3e8b4b2b6d
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-19 18:07:15 +0100

    created cucumber feature regarding #80

--- UPtables/app/models/room.rb
+++ UPtables/app/models/room.rb
@@ -21,6 +21,12 @@ class Room < ActiveRecord::Base
     Room::Unavailability.where(:room => self, :weekday => weekday, :timeframe => timeframe).first_or_create
   end
 
+  def available_at?(weekday, timeframe)
+    weekday   = weekday.id   if weekday.respond_to? :id
+    timeframe = timeframe.id if timeframe.respond_to? :id
+    not unavailabilities.any? {|u| (u.weekday_id == weekday) && (u.timeframe_id == timeframe) }
+  end
+
 
   def update_unavailabilities(params)
     ActiveRecord::Base.transaction do
