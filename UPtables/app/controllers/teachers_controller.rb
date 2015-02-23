ea320c5d1208285efabd1bc85bf699986e717117
Felix Kubicek <kubicek@uni-potsdam.de>
2015-02-23 12:51:18 +0100

    add teachers

--- UPtables/app/controllers/teachers_controller.rb
+++ UPtables/app/controllers/teachers_controller.rb
@@ -1,31 +1,39 @@
 class TeachersController < ApplicationController
   before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+  before_action :set_timeframes_weekdays, only: [:new, :edit]
+  
   def index
     @teachers = Teacher.all
   end
   
-  
-  def edit
-    @weekdays = Weekday.all
-    @timeframes = Timeframe.all
-    
-    @unavailabilities = Teacher::Unavailability.where(teacher:@teacher)
+  def new
+    @teacher = Teacher.new
   end
   
-  def update
+  def create
+    @teacher = Teacher.new(teacher_params)
     
-    @weekdays = Weekday.all
-    @timeframes = Timeframe.all
-        
-    respond_to do |format|
-      #@teacher.update(teacher_params)
-      if  @teacher.update_unavailabilities(params[:teacher_unavailability_ids])
-        format.html { redirect_to edit_teacher_path, notice: 'Teacher was successfully updated.' }
-      else
-        format.html { render :edit }
-        format.json { render json: @teacher.errors, status: :unprocessable_entity }
-      end
+    if @teacher.save and @teacher.update_unavailabilities(params[:teacher_unavailability_ids])
+      redirect_to teachers_path, notice: 'Teacher was successfully created.'
+    else
+      render :new
     end
+  end
+  
+  def edit
+  end
+  
+  def update      
+    if @teacher.update(teacher_params) and @teacher.update_unavailabilities(params[:teacher_unavailability_ids])
+      redirect_to edit_teacher_path, notice: 'Teacher was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @teacher.destroy
+    redirect_to teachers_url, notice: 'Teacher was successfully destroyed.'
   end
 
   private
@@ -35,5 +43,10 @@ class TeachersController < ApplicationController
     
     def set_teacher
       @teacher = Teacher.find(params[:id])
+    end
+    
+    def set_timeframes_weekdays
+      @weekdays = Weekday.all
+      @timeframes = Timeframe.all
     end
 end
--- UPtables/spec/models/teacher_spec.rb
+++ UPtables/spec/models/teacher_spec.rb
@@ -5,48 +5,58 @@ describe Teacher, type: :model do
 
   describe "#update_unavailabilities" do
     
-
-
-    it "creates unavailabilities" do
-      3.times do |i|
-        create :weekday, :id => i
-        create :timeframe, :id => i
+    context "update array nil" do
+      it "raises no error" do
+        update_array = nil
+        3.times {create :teacher_unavailability, :teacher_id => teacher.id}
+        expect {teacher.update_unavailabilities(update_array)}.to_not raise_error
       end
-      update_array = ["0 0", "1 1", "2 2"]
       
-      expect{teacher.update_unavailabilities(update_array)}.to change {Teacher::Unavailability.all.map{|u| [u.weekday_id, u.timeframe_id]}}.from([]).to([[0,0],[1,1],[2,2]])
-      
+      it "has no effect on availibilities" do
+        update_array = nil
+        3.times {create :teacher_unavailability, :teacher_id => teacher.id}
+        expect {teacher.update_unavailabilities(update_array)}.to_not change {Teacher::Unavailability.count}
+      end
     end
     
-    it "deletes unavailabilities" do
+    context "valid update array" do
+      it "creates unavailabilities" do
+        3.times do |i|
+          create :weekday, :id => i
+          create :timeframe, :id => i
+        end
+        update_array = ["0 0", "1 1", "2 2"]
+        
+        expect{teacher.update_unavailabilities(update_array)}.to change {Teacher::Unavailability.all.map{|u| [u.weekday_id, u.timeframe_id]}}.from([]).to([[0,0],[1,1],[2,2]])
+        
+      end
       
-      update_array = []
+      it "deletes unavailabilities" do
+        update_array = []
+        3.times {create :teacher_unavailability, :teacher_id => teacher.id}
+        expect{teacher.update_unavailabilities(update_array)}.to change {Teacher::Unavailability.count}.from(3).to(0)
+        
+      end
       
-      3.times {create :teacher_unavailability, :teacher_id => teacher.id}
-      
-      expect{teacher.update_unavailabilities(update_array)}.to change {Teacher::Unavailability.count}.from(3).to(0)
-      
-    end
-    
-    it "deletes only specified unavailabilities" do
-      
-      update_array = ["2 1", "2 4"]
-      
-      w2 = create :weekday, :id => 2
-      w5 = create :weekday, :id => 5
-      w6 = create :weekday, :id => 6
-      
-      t1 = create :timeframe, :id => 1
-      t4 = create :timeframe, :id => 4
-      
-      create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w2, :timeframe => t1
-      create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w2, :timeframe => t4
-      
-      create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w5, :timeframe => t1
-      create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w6, :timeframe => t4
-            
-      expect{teacher.update_unavailabilities(update_array)}.to change {Teacher::Unavailability.count}.from(4).to(2)
-      
+      it "deletes only specified unavailabilities" do
+        
+        update_array = ["2 1", "2 4"]
+        
+        w2 = create :weekday, :id => 2
+        w5 = create :weekday, :id => 5
+        w6 = create :weekday, :id => 6
+        
+        t1 = create :timeframe, :id => 1
+        t4 = create :timeframe, :id => 4
+        
+        create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w2, :timeframe => t1
+        create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w2, :timeframe => t4
+        
+        create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w5, :timeframe => t1
+        create :teacher_unavailability, :teacher_id => teacher.id, :weekday => w6, :timeframe => t4
+              
+        expect{teacher.update_unavailabilities(update_array)}.to change {Teacher::Unavailability.count}.from(4).to(2)
+      end
     end
     
   end
