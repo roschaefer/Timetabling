864748233c32a249e0f64cc4f7725b6ddb3503c3
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-02-26 15:11:28 +0100

    added controller specs and one model spec for ects_module

--- UPtables/spec/controllers/ects_modules_controller_spec.rb
+++ UPtables/spec/controllers/ects_modules_controller_spec.rb
@@ -18,27 +18,27 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe EtcsModulesController, type: :controller do
+RSpec.describe EctsModulesController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
-  # EtcsModule. As you add validations to EtcsModule, be sure to
+  # EctsModule. As you add validations to EctsModule, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {:name => "I am a module"}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {:name => nil}
   }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
-  # EtcsModulesController. Be sure to keep this updated too.
+  # EctsModulesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET #index" do
     it "assigns all ects_modules as @ects_modules" do
-      ects_module = EtcsModule.create! valid_attributes
+      ects_module = EctsModule.create! valid_attributes
       get :index, {}, valid_session
       expect(assigns(:ects_modules)).to eq([ects_module])
     end
@@ -46,7 +46,7 @@ RSpec.describe EtcsModulesController, type: :controller do
 
   describe "GET #show" do
     it "assigns the requested ects_module as @ects_module" do
-      ects_module = EtcsModule.create! valid_attributes
+      ects_module = EctsModule.create! valid_attributes
       get :show, {:id => ects_module.to_param}, valid_session
       expect(assigns(:ects_module)).to eq(ects_module)
     end
@@ -55,13 +55,13 @@ RSpec.describe EtcsModulesController, type: :controller do
   describe "GET #new" do
     it "assigns a new ects_module as @ects_module" do
       get :new, {}, valid_session
-      expect(assigns(:ects_module)).to be_a_new(EtcsModule)
+      expect(assigns(:ects_module)).to be_a_new(EctsModule)
     end
   end
 
   describe "GET #edit" do
     it "assigns the requested ects_module as @ects_module" do
-      ects_module = EtcsModule.create! valid_attributes
+      ects_module = EctsModule.create! valid_attributes
       get :edit, {:id => ects_module.to_param}, valid_session
       expect(assigns(:ects_module)).to eq(ects_module)
     end
@@ -69,28 +69,28 @@ RSpec.describe EtcsModulesController, type: :controller do
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new EtcsModule" do
+      it "creates a new EctsModule" do
         expect {
           post :create, {:ects_module => valid_attributes}, valid_session
-        }.to change(EtcsModule, :count).by(1)
+        }.to change(EctsModule, :count).by(1)
       end
 
       it "assigns a newly created ects_module as @ects_module" do
         post :create, {:ects_module => valid_attributes}, valid_session
-        expect(assigns(:ects_module)).to be_a(EtcsModule)
+        expect(assigns(:ects_module)).to be_a(EctsModule)
         expect(assigns(:ects_module)).to be_persisted
       end
 
       it "redirects to the created ects_module" do
         post :create, {:ects_module => valid_attributes}, valid_session
-        expect(response).to redirect_to(EtcsModule.last)
+        expect(response).to redirect_to(EctsModule.last)
       end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved ects_module as @ects_module" do
         post :create, {:ects_module => invalid_attributes}, valid_session
-        expect(assigns(:ects_module)).to be_a_new(EtcsModule)
+        expect(assigns(:ects_module)).to be_a_new(EctsModule)
       end
 
       it "re-renders the 'new' template" do
@@ -103,24 +103,26 @@ RSpec.describe EtcsModulesController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {:name => "Module with some courses", :course_ids => [23, 42]}
       }
 
       it "updates the requested ects_module" do
-        ects_module = EtcsModule.create! valid_attributes
+        ects_module = EctsModule.create! valid_attributes
+        create :course, :id => 23
+        create :course, :id => 42
         put :update, {:id => ects_module.to_param, :ects_module => new_attributes}, valid_session
         ects_module.reload
-        skip("Add assertions for updated state")
+        expect(ects_module.courses).to have(2).items
       end
 
       it "assigns the requested ects_module as @ects_module" do
-        ects_module = EtcsModule.create! valid_attributes
+        ects_module = EctsModule.create! valid_attributes
         put :update, {:id => ects_module.to_param, :ects_module => valid_attributes}, valid_session
         expect(assigns(:ects_module)).to eq(ects_module)
       end
 
       it "redirects to the ects_module" do
-        ects_module = EtcsModule.create! valid_attributes
+        ects_module = EctsModule.create! valid_attributes
         put :update, {:id => ects_module.to_param, :ects_module => valid_attributes}, valid_session
         expect(response).to redirect_to(ects_module)
       end
@@ -128,13 +130,13 @@ RSpec.describe EtcsModulesController, type: :controller do
 
     context "with invalid params" do
       it "assigns the ects_module as @ects_module" do
-        ects_module = EtcsModule.create! valid_attributes
+        ects_module = EctsModule.create! valid_attributes
         put :update, {:id => ects_module.to_param, :ects_module => invalid_attributes}, valid_session
         expect(assigns(:ects_module)).to eq(ects_module)
       end
 
       it "re-renders the 'edit' template" do
-        ects_module = EtcsModule.create! valid_attributes
+        ects_module = EctsModule.create! valid_attributes
         put :update, {:id => ects_module.to_param, :ects_module => invalid_attributes}, valid_session
         expect(response).to render_template("edit")
       end
@@ -143,14 +145,14 @@ RSpec.describe EtcsModulesController, type: :controller do
 
   describe "DELETE #destroy" do
     it "destroys the requested ects_module" do
-      ects_module = EtcsModule.create! valid_attributes
+      ects_module = EctsModule.create! valid_attributes
       expect {
         delete :destroy, {:id => ects_module.to_param}, valid_session
-      }.to change(EtcsModule, :count).by(-1)
+      }.to change(EctsModule, :count).by(-1)
     end
 
     it "redirects to the ects_modules list" do
-      ects_module = EtcsModule.create! valid_attributes
+      ects_module = EctsModule.create! valid_attributes
       delete :destroy, {:id => ects_module.to_param}, valid_session
       expect(response).to redirect_to(ects_modules_url)
     end
--- UPtables/spec/models/ects_module_spec.rb
+++ UPtables/spec/models/ects_module_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe EctsModule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:ects_module) { create :ects_module }
+  describe "#update" do
+    it "updates many to many association on courses" do
+      create :course, :id => 3
+      create :course, :id => 7
+      ects_module.update(:course_ids => [3, 7])
+      expect(ects_module.courses).to have(2).items
+    end
+  end
 end
