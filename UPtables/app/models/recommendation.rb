39c42bee9778b606327af197bc7334f6b1f0b7f7
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-03-01 21:39:37 +0100

    added custom validation, recommendation only valid if course belongs to curriculum

--- UPtables/app/models/recommendation.rb
+++ UPtables/app/models/recommendation.rb
@@ -1,4 +1,13 @@
 class Recommendation < ActiveRecord::Base
   belongs_to :course
   belongs_to :curriculum
+  validates :semester, :course_id, :curriculum_id, presence: true
+  validate :course_belongs_to_curriculum
+
+
+  def course_belongs_to_curriculum
+    unless course.curricula.include?(self.curriculum)
+      errors.add(:course, "doesn't belong to the given curriculum")
+    end
+  end
 end
--- UPtables/spec/models/recommendation_spec.rb
+++ UPtables/spec/models/recommendation_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Recommendation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build :recommendation, attributes }
+  let(:attributes) { { } }
+  describe "#valid?" do
+    context "without semester" do
+      let(:attributes) { super().merge(:semester => nil) }
+      it { is_expected.not_to be_valid}
+    end
+
+    context "course <=> ects module <=> curriculum" do
+      let(:course)          { create :course }
+      let(:curriculum)      { create :curriculum }
+      let(:attributes) { super().merge(:course => course, :curriculum => curriculum) }
+
+      context "course doesn't belong to curriculum" do
+        it { is_expected.not_to be_valid}
+      end
+
+      context "course belongs to curriculum" do
+        before {
+          create(:ects_module, :courses => [course], :curricula => [curriculum])
+        }
+        it { is_expected.to be_valid}
+      end
+    end
+
+  end
 end
