abba5311f40629edc8d6a37faa59532c30ea75aa
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-03-03 11:15:40 +0100

    reorganized features, added empty cucumber feature for mandatory courses

---
+++ UPtables/features/step_definitions/asp_steps.rb
@@ -0,0 +1,44 @@
+Angenommen(/^unser Stundenplan sieht so aus:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Angenommen(/^es gibt die zwei Studienordnungen "(.*?)" und "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Angenommen(/^es gibt die Kurse "(.*?)" und "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Angenommen(/^die Kurse sind beides Pflichtveranstaltungen im Studiengang "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Angenommen(/^die Kurse sind beide im (\d+)\. Semester empfohlen$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^gibt es keine Lösung, weil sich die Kurse nicht überschneiden dürfen$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Angenommen(/^der Kurs "(.*?)" ist im (\d+)\. Semester empfohlen$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^gibt es genau eine Lösung$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^diese Lösung hat gewisse Kosten wegen Überschneidungen im gleichen Studiengang$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Angenommen(/^der Kurs "(.*?)" ist im Studiengang "(.*?)" Pflicht$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Dann(/^diese Lösung hat sogar gar keine Kosten, weil es keine Überschneidungen gibt$/) do
+  pending # express the regexp above with the code you wish you had
+end
