abf123d1679c19b1b032bc3894b4a8608c50cc6f
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-03-03 12:42:01 +0100

    removed unnecessary isolated lectures contraint

--- UPtables/lib/asp/constraint.rb
+++ UPtables/lib/asp/constraint.rb
@@ -1,7 +1,7 @@
 class Asp::Constraint
 
   HARD_CONSTRAINTS = ["scheduled", "conflicts", "room_occupancy", "availability", "double_lectures", "additional", "room_availability"]
-  SOFT_CONSTRAINTS = ["room_capacity", "minimum_working_days", "isolated_lectures"]
+  SOFT_CONSTRAINTS = ["room_capacity", "minimum_working_days"]
 
   class << self
     protected :new
