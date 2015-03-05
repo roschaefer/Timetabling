989ec311025b5dee314c0a41a9165010db19059b
Felix Kubicek <kubicek@uni-potsdam.de>
2015-03-05 12:53:47 +0100

    clear migration history

--- UPtables/db/migrate/20150303115013_rename_curricula_ects_modules_to_curriculum_module_assignments.rb
+++
@@ -1,5 +0,0 @@
-class RenameCurriculaEctsModulesToCurriculumModuleAssignments < ActiveRecord::Migration
-  def change
-    rename_table :curricula_ects_modules, :curriculum_module_assignments
-  end
-end
--- UPtables/db/migrate/20150303120326_add_mandatory_to_curriculum_module_assignments.rb
+++
@@ -1,5 +0,0 @@
-class AddMandatoryToCurriculumModuleAssignments < ActiveRecord::Migration
-  def change
-    add_column :curriculum_module_assignments, :mandatory, :boolean, :default => false
-  end
-end
