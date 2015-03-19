c14291152a3373ac8bb3a87b03fc7f24e41a4ad1
Felix Kubicek <kubicek@uni-potsdam.de>
2015-03-19 14:48:29 +0100

    forgot to add model

---
+++ UPtables/app/models/course/component.rb
@@ -0,0 +1,19 @@
+class Course::Component < ActiveRecord::Base
+  
+  TYPES = ['Vorlesung', 'Übung', 'Seminar', 'Projekt']
+  
+  belongs_to :teacher
+  belongs_to :course
+  
+  validates :dates, :minimum_working_days, :numericality => { :greater_than => 0}
+  validates  :course, :teacher, :dates,  presence: true
+  validates  :type, inclusion: { in: TYPES }
+  
+  self.inheritance_column = nil
+  
+  def to_fact
+    dl = (double_lecture && "1") || "0"
+    "course_component(#{id},#{course_id},#{type},#{teacher_id},#{dates},#{minimum_working_days},#{dl})."
+  end
+  
+end
