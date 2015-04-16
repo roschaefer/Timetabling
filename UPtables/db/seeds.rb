1cbc004948f5ca5779616af00e9e46e87a295fb1
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-04-16 11:37:20 +0200

    intermediate step, created hand-tailored seeds for presentation

--- UPtables/db/seeds.rb
+++ UPtables/db/seeds.rb
@@ -7,134 +7,117 @@
 #   Mayor.create!(name: 'Emanuel', city: cities.first)
 
 # Rooms have correspondant id in facts.lp !!
-Room.create!(:id => 1, :name => "0.02", :capacity => 32)
-Room.create!(:id => 2, :name => "0.03", :capacity => 32)
-Room.create!(:id => 3, :name => "0.04", :capacity => 32)
-Room.create!(:id => 4, :name => "1.02", :capacity => 32)
-Room.create!(:id => 5, :name => "1.03", :capacity => 32)
-Room.create!(:id => 6, :name => "2.01", :capacity => 16)
-Room.create!(:id => 7, :name => "2.14", :capacity => 12)
-h01 = Room.create!(:id => 8, :name => "H01",  :capacity =>100)
+room_1 = Room.create!(:id => 1, :name => "Room 1", :capacity => 32)
+room_2 = Room.create!(:id => 2, :name => "Room 2", :capacity => 32)
+room_3 = Room.create!(:id => 3, :name => "Room 3", :capacity => 16)
+room_4 = Room.create!(:id => 4, :name => "Room 4", :capacity => 12)
+h01 = Room.create!(:id => 5, :name => "H01",  :capacity => 80)
 
 #Weekdays have correspondant predicates in encoding with corresponding ids!
 monday = Weekday.create!(:id => 0, :name => "Montag")
-Weekday.create!(:id => 1, :name => "Dienstag" )
-Weekday.create!(:id => 2, :name => "Mittwoch" )
-Weekday.create!(:id => 3, :name => "Donnerstag" )
-Weekday.create!(:id => 4, :name => "Freitag" )
+tuesday = Weekday.create!(:id => 1, :name => "Dienstag" )
+wednesday = Weekday.create!(:id => 2, :name => "Mittwoch" )
 
 #Timeframes have correspondant predicates in encoding with corresponding ids!
 eight_o_clock = Timeframe.create!(:id => 0, :interval => "08:00 - 10:00 ")
 Timeframe.create!(:id => 1, :interval => "10:00 - 12:00" )
 Timeframe.create!(:id => 2, :interval => "12:00 - 14:00" )
-Timeframe.create!(:id => 3, :interval => "14:00 - 16:00" )
-Timeframe.create!(:id => 4, :interval => "16:00 - 18:00" )
-Timeframe.create!(:id => 5, :interval => "18:00 - 20:00" )
 
-schaub         =  Teacher.create!(:id => 0,  :first_name => "Torsten",   :surname =>"Schaub")
-romero         =  Teacher.create!(:id => 1,  :first_name => "Javier",    :surname =>"Romero")
-schnor         =  Teacher.create!(:id => 2,  :first_name => "Bettina",   :surname =>"Schnor")
-bordihn        =  Teacher.create!(:id => 3,  :first_name => "Henning",   :surname =>"Bordihn")
-scheffer       =  Teacher.create!(:id => 4,  :first_name => "Tobias",    :surname =>"Scheffer")
-voigt          =  Teacher.create!(:id => 5,  :first_name => "Horst",     :surname =>"Voigt")
-lamprecht      =  Teacher.create!(:id => 6,  :first_name => "Anna-Lena", :surname =>"Lamprecht")
-lucke          =  Teacher.create!(:id => 7,  :first_name => "Ulrike",    :surname =>"Lucke")
-vogel          =  Teacher.create!(:id => 8,  :first_name => "Petra",     :surname =>"Vogel")
-schwill        =  Teacher.create!(:id => 9,  :first_name => "Andreas",   :surname =>"Schwill")
-kiertscher     =  Teacher.create!(:id => 10, :first_name => "Simon",     :surname =>"Kiertscher")
-reich          =  Teacher.create!(:id => 11, :first_name => "Sebastian", :surname =>"Reich")
-fiedler        =  Teacher.create!(:id => 12, :first_name => "Bernhard",  :surname =>"Fiedler")
-kreitz         =  Teacher.create!(:id => 13, :first_name => "Christoph", :surname =>"Kreitz")
-gordon         =  Teacher.create!(:id => 14, :first_name => "Thomas",    :surname =>"Gordon")
-kalkbrenner    =  Teacher.create!(:id => 15, :first_name => "Gerrit",    :surname =>"Kalkbrenner")
-blanchard      =  Teacher.create!(:id => 16, :first_name => "Gilles",    :surname =>"Blanchard")
-behr           =  Teacher.create!(:id => 17, :first_name => "Peter",     :surname =>"Behr")
-zinke          =  Teacher.create!(:id => 18, :first_name => "Francis",   :surname =>"Zinke")
-koehlmann      =  Teacher.create!(:id => 19, :first_name => "Wiebke",    :surname =>"Köhlmann")
-goessel        =  Teacher.create!(:id => 20, :first_name => "Michael",   :surname =>"Gössel")
+math_prof         = Teacher.create!(:id => 1,  :first_name => "Math",   :surname         =>"Prof")
+stochastic_prof   = Teacher.create!(:id => 2,  :first_name => "Stochastic",   :surname   =>"Prof")
+programming_prof  = Teacher.create!(:id => 3,  :first_name => "Programming",   :surname  =>"Prof")
+programming_tutor = Teacher.create!(:id => 4,  :first_name => "Programming",    :surname =>"Tutor")
 
+
+
+# MATH CURRICULUM
+math_curriculum =  Curriculum.create!(:name => "Mathematics Bachelor")
+math_basics =  EctsModule.create!(:name => "Basics of Mathematics")
+CurriculumModuleAssignment.create!(:ects_module => math_basics, :curriculum => math_curriculum, :mandatory => true)
+stochastic_basics =  EctsModule.create!(:name => "Basics of Stochastic")
+CurriculumModuleAssignment.create!(:ects_module => stochastic_basics, :curriculum => math_curriculum, :mandatory => true)
+
+mathematik1  = Course.create!(:name => "Mathematics I", :teacher => math_prof)
+mathematik2  = Course.create!(:name => "Mathematics II", :teacher => math_prof)
+mathematik3  = Course.create!(:name => "Mathematics III", :teacher => math_prof)
+Course::Component.create!(:course => mathematik1 , :type => :lecture      , :teacher => math_prof      , :dates => 1, :double_lecture => false, :participants => 50)
+Course::Component.create!(:course => mathematik2 , :type => :lecture      , :teacher => math_prof      , :dates => 1, :double_lecture => false, :participants => 50)
+Course::Component.create!(:course => mathematik3 , :type => :lecture      , :teacher => math_prof      , :dates => 1, :double_lecture => false, :participants => 30)
+
+mathematik1.ects_modules << math_basics
+mathematik2.ects_modules << math_basics
+mathematik3.ects_modules << math_basics
+
+stochastic1  = Course.create!(:name => "Stochastic I", :teacher => stochastic_prof)
+stochastic2  = Course.create!(:name => "Stochastic II", :teacher => stochastic_prof)
+stochastic3  = Course.create!(:name => "Stochastic III", :teacher => stochastic_prof)
+Course::Component.create!(:course => stochastic1 , :type => :lecture      , :teacher => stochastic_prof      , :dates => 1, :double_lecture => false, :participants => 50)
+Course::Component.create!(:course => stochastic2 , :type => :lecture      , :teacher => stochastic_prof      , :dates => 1, :double_lecture => false, :participants => 20)
+Course::Component.create!(:course => stochastic3 , :type => :lecture      , :teacher => stochastic_prof      , :dates => 1, :double_lecture => false, :participants => 10)
+
+stochastic1.ects_modules << stochastic_basics
+stochastic2.ects_modules << stochastic_basics
+stochastic3.ects_modules << stochastic_basics
+
+
+# COMPUTER DCIENCE CURRICULUM
+computer_science_curriculum  =  Curriculum.create!(:name => "Computer Science Bachelor")
+programming_basics = EctsModule.create!(:name => "Basics of Programming")
+CurriculumModuleAssignment.create!(:ects_module => programming_basics, :curriculum => computer_science_curriculum, :mandatory => true)
+advanced_programming = EctsModule.create!(:name => "Advanced Programming")
+CurriculumModuleAssignment.create!(:ects_module => advanced_programming, :curriculum => computer_science_curriculum, :mandatory => false)
+# take care, this actually belongs to section match curriculum
+CurriculumModuleAssignment.create!(:ects_module => math_basics, :curriculum => computer_science_curriculum, :mandatory => true)
+
+programming1  = Course.create!(:name => "Programming I", :teacher => programming_prof)
+programming2  = Course.create!(:name => "Programming II", :teacher => programming_prof)
+programming1.ects_modules << programming_basics
+programming2.ects_modules << programming_basics
+
+Course::Component.create!(:course => programming1 , :type => :lecture      , :teacher =>  programming_prof, :dates => 1, :double_lecture => false, :participants => 50)
+Course::Component.create!(:course => programming2 , :type => :lecture      , :teacher =>  programming_prof, :dates => 1, :double_lecture => false, :participants => 30)
+Course::Component.create!(:course => programming1 , :type => :tutorial      , :teacher =>  programming_tutor, :dates => 2, :double_lecture => false, :participants => 25)
+Course::Component.create!(:course => programming2 , :type => :tutorial      , :teacher =>  programming_tutor, :dates => 2, :double_lecture => false, :participants => 15)
+
+java_programming = Course.create!(:name => "Java Programming", :teacher => programming_prof)
+ruby_programming = Course.create!(:name => "Ruby Programming", :teacher => programming_prof)
+python_programming = Course.create!(:name => "Python Programming", :teacher => programming_prof)
+java_programming.ects_modules << advanced_programming
+ruby_programming.ects_modules << advanced_programming
+python_programming.ects_modules << advanced_programming
+
+Course::Component.create!(:course => java_programming , :type => :tutorial, :teacher =>  programming_tutor, :dates => 1, :double_lecture => false, :participants => 15)
+Course::Component.create!(:course => ruby_programming , :type => :tutorial, :teacher =>  programming_tutor, :dates => 1, :double_lecture => false, :participants => 15)
+Course::Component.create!(:course => python_programming , :type => :tutorial, :teacher =>  programming_tutor, :dates => 1, :double_lecture => false, :participants => 15)
+
+# CURRICLUM OF TEACHER TAINING
+
+
+
+# Room unavailabilities and properties
+Room::Unavailability.create!(:room => h01, :weekday => monday, :timeframe => eight_o_clock)
+Room::Unavailability.create!(:room => h01, :weekday => tuesday, :timeframe => eight_o_clock)
+Room::Unavailability.create!(:room => h01, :weekday => wednesday, :timeframe => eight_o_clock)
+
+beamer = Room::Property.create(:name => "Beamer")
+computer_pool = Room::Property.create(:name => "Computer pool")
+
+room_3.properties << computer_pool
+room_4.properties << computer_pool
+java_programming.components.first.required_room_properties << computer_pool
+ruby_programming.components.first.required_room_properties << computer_pool
+python_programming.components.first.required_room_properties << computer_pool
 
 # Unavailabilities of teaching persons
-Weekday.find(0,1,2,3).each do |w|
+[monday, tuesday].each do |w|
   Timeframe.all.each do |t|
-    Teacher::Unavailability.create!(:teacher => schaub, :weekday => w, :timeframe => t)
+    Teacher::Unavailability.create!(:teacher => programming_prof, :weekday => w, :timeframe => t)
   end
 end
 
-courses = []
-courses[0]  = Course.create!(:name => "Informatik für Naturwissenschaftler I"                           , :teacher => voigt      )
-courses[1]  = Course.create!(:name => "Grundlagen der Informationsverarbeitung"                         , :teacher => lucke      )
-courses[2]  = Course.create!(:name => "Grundlagen der Programmierung"                                   , :teacher => schwill    )
-courses[3]  = Course.create!(:name => "Mathematik für Informatiker I"                                   , :teacher => reich      )
-courses[4]  = Course.create!(:name => "Theoretische Informatik I"                                       , :teacher => kreitz     )
-courses[5]  = Course.create!(:name => "Argumentation Systems Engineering"                               , :teacher => gordon     )
-courses[6]  = Course.create!(:name => "Automotiv, Fahrassistenz und Selbstfahrer"                       , :teacher => kalkbrenner)
-courses[7]  = Course.create!(:name => "ClusterComputing Fachseminar"                                    , :teacher => schnor     )
-courses[8]  = Course.create!(:name => "Face Recognition"                                                , :teacher => scheffer   )
-courses[9]  = Course.create!(:name => "Grundlagen der Betriebssysteme und Rechnernetze"                 , :teacher => schnor     )
-courses[10] = Course.create!(:name => "Grundlagen der Stochastik"                                       , :teacher => blanchard  )
-courses[11] = Course.create!(:name => "Mikroprozessortechnik"                                           , :teacher => behr       )
-courses[12] = Course.create!(:name => "Multimedia-Technologie"                                          , :teacher => lucke      )
-courses[13] =  Course.create!(:name => "Pimp your video conference!"                                    , :teacher => koehlmann  )
 
-#courses << Course.create!(:name => "Software Engineering I Vorlesung"                                , :teacher => bordihn    , :dates => 1, :participants => 75 )
-#courses << Course.create!(:name => "Software Engineering I Übung"                                    , :teacher => bordihn    , :dates => 3, :participants => 25 )
-#courses << Course.create!(:name => "Software Engineering I Projekt"                                  , :teacher => voigt      , :dates => 1, :participants => 75 )
-#courses << Course.create!(:name => "Wissenschaftliche Workflows Proseminar"                          , :teacher => lamprecht  , :dates => 1, :participants => 12 )
-#courses << Course.create!(:name => "Absicherung von IPv6-Netzen Fachseminar"                         , :teacher => schnor     , :dates => 1, :participants => 10 )
-#courses << Course.create!(:name => "Agententechnologie/Agent-technology Seminar"                     , :teacher => schaub     , :dates => 1, :participants => 10 )
-#courses << Course.create!(:name => "Cartesisches Seminar Oberseminar"                                , :teacher => kreitz     , :dates => 1, :participants => 12 )
-#courses << Course.create!(:name => "Codierungstheorie 2 Seminar"                                     , :teacher => goessel    , :dates => 1, :participants => 10 )
-#courses << Course.create!(:name => "Deklarative Problemlösung und Optimierung Vorlesung"             , :teacher => schaub     , :dates => 2, :participants => 10 )
-#courses << Course.create!(:name => "Deklarative Problemlösung und Optimierung Übung"                 , :teacher => romero     , :dates => 1, :participants => 10 )
-#courses << Course.create!(:name => "Current Topics in Computational Intelligence Seminar"            , :teacher => schaub     , :dates => 1, :participants => 10 )
+# ects_module1 =  EctsModule.create!(:name => "Grundlagen der Programmierung")
+# curricula1 =  Curriculum.create!(:name => "Informatik Bachelor")
+# curricula1.ects_modules << ects_module1 
 
 
-Course::Component.create!(:course => courses[0] , :type => :lecture      , :teacher => voigt       , :dates => 1, :double_lecture => false, :participants => 100)
-Course::Component.create!(:course => courses[0] , :type => :tutorial     , :teacher => lamprecht   , :dates => 1, :double_lecture => false, :participants => 25)
-Course::Component.create!(:course => courses[1] , :type => :lecture      , :teacher => lucke       , :dates => 1, :double_lecture => false, :participants => 100)
-Course::Component.create!(:course => courses[1] , :type => :tutorial     , :teacher => vogel       , :dates => 2, :double_lecture => false, :participants => 12)
-Course::Component.create!(:course => courses[2] , :type => :lecture      , :teacher => schwill     , :dates => 2, :double_lecture => false, :participants => 100)
-Course::Component.create!(:course => courses[2] , :type => :tutorial     , :teacher => kiertscher  , :dates => 6, :double_lecture => false, :participants => 10)
-Course::Component.create!(:course => courses[3] , :type => :lecture      , :teacher => reich       , :dates => 1, :double_lecture => false, :participants => 200)
-Course::Component.create!(:course => courses[3] , :type => :tutorial     , :teacher => fiedler     , :dates => 2, :double_lecture => false, :participants => 10)
-Course::Component.create!(:course => courses[4] , :type => :lecture      , :teacher => kreitz      , :dates => 1, :double_lecture => false, :participants => 200)
-Course::Component.create!(:course => courses[4] , :type => :tutorial     , :teacher => kreitz      , :dates => 4, :double_lecture => false, :participants => 10)
-Course::Component.create!(:course => courses[5] , :type => :lecture      , :teacher => gordon      , :dates => 1, :double_lecture => false, :participants => 40)
-Course::Component.create!(:course => courses[6] , :type => :seminar      , :teacher => kalkbrenner , :dates => 1, :double_lecture => false, :participants => 20)
-Course::Component.create!(:course => courses[7] , :type => :seminar      , :teacher => schnor      , :dates => 1, :double_lecture => false, :participants => 20)
-Course::Component.create!(:course => courses[8] , :type => :seminar      , :teacher => scheffer    , :dates => 1, :double_lecture => false, :participants => 10)
-Course::Component.create!(:course => courses[9] , :type => :lecture      , :teacher => schnor      , :dates => 1, :double_lecture => false, :participants => 30)
-Course::Component.create!(:course => courses[9] , :type => :tutorial     , :teacher => schnor      , :dates => 1, :double_lecture => false, :participants => 30)
-Course::Component.create!(:course => courses[10], :type => :lecture      , :teacher => blanchard   , :dates => 1, :double_lecture => false, :participants => 30)
-Course::Component.create!(:course => courses[10], :type => :tutorial     , :teacher => blanchard   , :dates => 1, :double_lecture => false, :participants => 30)
-Course::Component.create!(:course => courses[11], :type => :lecture      , :teacher => behr        , :dates => 1, :double_lecture => false, :participants => 20)
-Course::Component.create!(:course => courses[12], :type => :lecture      , :teacher => lucke       , :dates => 1, :double_lecture => false, :participants => 20)
-Course::Component.create!(:course => courses[12], :type => :tutorial     , :teacher => zinke       , :dates => 1, :double_lecture => false, :participants => 10)
-Course::Component.create!(:course => courses[13], :type => :project      , :teacher => koehlmann   , :dates => 1, :double_lecture => false, :participants => 10)
-
-
-Room::Unavailability.create!(:room => h01, :weekday => monday, :timeframe => eight_o_clock)
-
-ects_modules = []
-ects_modules << EctsModule.create!(:name => "Grundlagen der Programmierung")
-ects_modules << EctsModule.create!(:name => "Algorithmen und Datenstrukturen")
-ects_modules << EctsModule.create!(:name => "Theoretische Grundlagen: Effiziente Algorithmen")
-ects_modules << EctsModule.create!(:name => "Konzepte paralleler Programmierung")
-
-curricula = []
-curricula << Curriculum.create!(:name => "Informatik Bachelor")
-curricula << Curriculum.create!(:name => "Informatik Master")
-curricula << Curriculum.create!(:name => "CS Bachelor")
-curricula << Curriculum.create!(:name => "CS Master")
-
-curricula.each do |c|
-  c.ects_modules << ects_modules.sample(Random.rand(ects_modules.size))
-end
-
-courses.each do |course|
-  course.ects_modules << ects_modules.sample(Random.rand(ects_modules.size))
-end
-
-Room::Property.create(:name => "Beamer")
-Room::Property.create(:name => "Computer pool")
